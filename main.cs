ee9d8a06b8480f88ccbfc5891089b351e81b9254
Alla <120055981+AllaVyatkina@users.noreply.github.com>
2023-01-31 16:25:53 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    //   Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+     int SumNumbers(int n, int m) 
+{ 
+     if (n != m) 
+     {
+         return (n+SumNumbers(n - 1, m)); 
+     } 
+  else return n;
+}
+    Console.Write("Введите число меньшее число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите число большее число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumNumbers(n, m); 
+Console.Write(sum);
+    }
+  }
+
+  }
+}
